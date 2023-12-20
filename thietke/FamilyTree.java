93c8fd7e5e0a2408e8802df23b3b8bb68d65799f
gadu04 <126551507+gadu04@users.noreply.github.com>
2023-12-21 00:32:27 +0700

    Update FamilyTree.java

--- thietke/FamilyTree.java
+++ thietke/FamilyTree.java
@@ -1,1 +1,16 @@
+public class FamilyTree {
+    public static void main(String[] args) {
+        // Creating individuals
+        Individual james = new Individual("James", "1970-01-01", "Male");
+        Individual hana = new Individual("Hana", "1972-05-12", "Female");
 
+        james.setMarried(true);
+        hana.setMarried(true);
+
+        Individual ryan = new Individual("Ryan", "1995-03-15", "Male");
+        Individual kai = new Individual("Kai", "1997-07-21", "Male");
+
+        james.addChild(ryan);
+        james.addChild(kai);
+    }
+}
